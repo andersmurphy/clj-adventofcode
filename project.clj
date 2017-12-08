cb63cadf760f0e97d4999d0cbdcab1092d3d15db
Anders <andersmurphy@gmail.com>
2017-12-08 18:50:57 +0000

    Add eastwood and kibit for linting

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject adventofcode "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[jonase/eastwood "0.2.5"]
+            [lein-kibit "0.1.5"]])
