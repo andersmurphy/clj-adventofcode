f3f154615058e87acc91eb7efb5e62c67acd355a
Anders <andersmurphy@gmail.com>
2017-12-09 16:34:49 +0000

    Add cljfmt for checking code format

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject adventofcode "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :plugins [[jonase/eastwood "0.2.5"]
-            [lein-kibit "0.1.5"]])
+            [lein-kibit "0.1.5"]
+            [lein-cljfmt "0.5.7"]])
