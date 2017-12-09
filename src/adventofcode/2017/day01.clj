6e502e8e689402a5309abf32a70d23fb6b36a309
Anders <andersmurphy@gmail.com>
2017-12-09 16:35:54 +0000

    Solution to day 01

--- src/adventofcode/2017/day01.clj
+++ src/adventofcode/2017/day01.clj
@@ -1,6 +1,22 @@
-(ns adventofcode.2017.day01)
+(ns adventofcode.2017.day01
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def input-string (string/trim-newline (slurp (io/resource "adventofcode/2017/day01"))))
+
+(defn convert-string-to-array-of-numbers
+  [put-string]
+  (map (comp read-string str) (seq input-string)))
+
+(defn count-neighbouring-numbers
+  [[head & tail] previous result]
+  (if-not head
+    result
+    (recur tail head (if (= head previous)
+                       (+ head result)
+                       result))))
+
+(defn solve-captcha
+  [input-string]
+  (let [array-of-numbers (convert-string-to-array-of-numbers input-string)]
+    (count-neighbouring-numbers array-of-numbers (last array-of-numbers) 0)))
--- test/adventofcode/2017/day01_test.clj
+++ test/adventofcode/2017/day01_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [adventofcode.2017.day01 :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-solve-captcha
+
+  (testing "returns 0 when input is an empty string"
+    (is (= (solve-captcha "") 0)))
+
+  (testing "1122 returns a sum of 3"
+    (is (= (solve-captcha "1122") 3)))
+
+  (testing "1111 returns a sum of 4"
+    (is (= (solve-captcha "1111") 4)))
+
+  (testing "1234 returns a sum of 0"
+    (is (= (solve-captcha "1234") 0)))
+
+  (testing "91212129 returns a sum of 9"
+    (is (= (solve-captcha "91212129") 9))))
