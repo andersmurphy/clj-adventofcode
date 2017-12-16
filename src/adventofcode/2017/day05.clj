2111bd498ee92bd06ae54af4e45b784edd018e00
Anders <andersmurphy@gmail.com>
2017-12-16 20:43:12 +0000

    Solution to day 05

---
+++ src/adventofcode/2017/day05.clj
@@ -0,0 +1,23 @@
+(ns adventofcode.2017.day05
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(def input-string (string/trim-newline (slurp (io/resource "adventofcode/2017/day05.txt"))))
+
+(defn maze-from-string
+  [input-string]
+  (mapv read-string (string/split input-string #"\n")))
+
+(defn next-step
+  [current-position maze steps]
+  (let [current-position-value (get maze current-position)]
+    (if-not current-position-value
+      steps
+      (recur
+       (+ current-position current-position-value)
+       (assoc maze current-position (inc current-position-value))
+       (inc steps)))))
+
+(defn solve-maze
+  [input-string]
+  (next-step 0 (maze-from-string input-string) 0))
