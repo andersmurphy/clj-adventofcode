b1d5a291c4a08e19da730539def8ac54589cf19e
anders <andersmurphy@gmail.com>
2018-10-06 17:17:59 +0100

    Delete tests

--- test/adventofcode/2017/day05_test.clj
+++
@@ -1,7 +0,0 @@
-(ns adventofcode.2017.day05-test
-  (:require [clojure.test :refer :all]
-            [adventofcode.2017.day05 :refer :all]))
-
-(deftest test-solve-maze
-  (testing "escapes maze in 5 steps"
-    (is (= (solve-maze "0\n3\n0\n1\n-3") 5))))
