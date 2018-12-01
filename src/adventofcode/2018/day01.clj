45cb3460d2e63b7f6124fdbfca53e832e578d4e5
anders <andersmurphy@gmail.com>
2018-12-01 21:46:37 +0000

    Solve day01 2018 part 2

--- src/adventofcode/2018/day01.clj
+++ src/adventofcode/2018/day01.clj
@@ -5,7 +5,24 @@
 (defn split-on-new-line [s]
   (str/split s #"\n"))
 
-(->> (slurp (io/resource "adventofcode/2018/day01.txt"))
-     split-on-new-line
-     (map #(Integer/parseInt %))
-     (apply +))
+(defn parse-file []
+  (->> (slurp (io/resource "adventofcode/2018/day01.txt"))
+       split-on-new-line
+       (map #(Integer/parseInt %))))
+
+(defn solve-1 []
+  (->> (parse-file)
+       (apply +)))
+
+(defn find-first-dup
+  ([frequencies]
+   (find-first-dup [#{} 0] (cycle frequencies)))
+  ([[seen old-freq] infseq]
+   (let [new-freq (+ old-freq (first infseq))]
+     (if (seen new-freq)
+       new-freq
+       (recur [(conj seen new-freq) new-freq] (drop 1 infseq))))))
+
+(defn solve-2 []
+  (-> (parse-file)
+      find-first-dup))
