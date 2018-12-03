63865bb643651c438102b2d842be84e16e40b7f0
anders <andersmurphy@gmail.com>
2018-12-03 21:09:59 +0000

    Solve day02 2018 part 2

--- src/adventofcode/2018/day02.clj
+++ src/adventofcode/2018/day02.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn parse-file []
+(def parsed-file
   (->> (slurp (io/resource "adventofcode/2018/day02.txt"))
        str/split-lines))
 
@@ -11,11 +11,23 @@
        (some (fn [[k v]] (if (= n v) {n 1})))))
 
 (defn solve-1 []
-  (->> (parse-file)
+  (->> parsed-file
        (map (fn [s] (into (some-n 2 s) (some-n 3 s))))
        (apply merge-with +)
        vals
        (apply *)))
 
+(defn remove-differing-characters [s1 s2]
+  (->> (map (fn [c1 c2] (if (= c1 c2) c1 "")) s1 s2)
+       str/join))
+
+(defn apply-to-all-pairs [f coll]
+  (for [s1 coll s2 coll] (f s1 s2)))
+
+(def id-length (-> parsed-file first count))
+
 (defn solve-2 []
-  nil)
+  (->> parsed-file
+       (apply-to-all-pairs remove-differing-characters)
+       (filter (fn [s] (= (count s) (dec id-length))))
+       first))
