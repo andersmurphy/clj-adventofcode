2bf342e9b91943eaff6d35416017645a4b02b3e2
anders <andersmurphy@gmail.com>
2018-12-05 22:48:10 +0000

    Solve day03 2018 part 2

--- src/adventofcode/2018/day03.clj
+++ src/adventofcode/2018/day03.clj
@@ -7,22 +7,28 @@
   (->> (slurp (io/resource "adventofcode/2018/day03.txt"))
        str/split-lines))
 
-(defn data-str->numbers [s]
-  (let [[id x y w h] (->> (re-seq #"\d+" s) (map #(Integer/parseInt %)))]
-    {:id id :x x :y y :w w :h h}))
+(defn s->claim [s]
+  (let [[id x y w h] (->> (re-seq #"\d+" s) (map #(Integer/parseInt %)))
+        tiles        (for [x (range x (+ x w)) y (range y (+ y h))]
+                       [x y])]
+    {:id id :x x :y y :w w :h h :tiles tiles}))
 
-(defn numbers->tiles [{:keys [id x y w h] :as m}]
-  (for [x (range x (+ x w)) y (range y (+ y h))]
-    [x y]))
+(defn find-overlapping [claims]
+  (->> (mapcat :tiles claims)
+       frequencies
+       (into #{} (comp
+                  (filter (fn [[_ v]] (> v 1)))
+                  (map first)))))
 
 (defn solve-1 []
-  (->> parsed-file
-       (map data-str->numbers)
-       (mapcat numbers->tiles)
-       frequencies
-       vals
-       (filter #(> % 1))
+  (->> (map s->claim parsed-file)
+       find-overlapping
        count))
 
 (defn solve-2 []
-  nil)
+  (let [claims      (map s->claim parsed-file)
+        overlapping (find-overlapping claims)]
+    (some (fn [{:keys [id tiles]}]
+            (when (empty? (intersection overlapping (set tiles)))
+              id))
+          claims)))
