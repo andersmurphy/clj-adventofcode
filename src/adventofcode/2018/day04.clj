ff69d4bd792f6f1dacfcb77271b3314282286929
anders <andersmurphy@gmail.com>
2018-12-08 23:01:48 +0000

    Solve day04 2018 part 2

--- src/adventofcode/2018/day04.clj
+++ src/adventofcode/2018/day04.clj
@@ -7,10 +7,9 @@
         (re-find #"\[(.*?)\] (Guard #(\d+))* *(\w+ \w+)" s)]
     {:time time :id id :event event}))
 
-(def parse-data
+(def parsed-data
   (->> (slurp (io/resource "adventofcode/2018/day04.txt"))
-       str/split-lines
-       (map data->m)))
+       str/split-lines (map data->m)))
 
 (defn populate-missing-ids [entries]
   (reduce (fn [entries {:keys [id] :as entry}]
@@ -29,23 +28,20 @@
 
 (defn merge-time [[id entries]]
   (reduce (fn [minutes {:keys [time]}]
-            (update minutes :time into time))
-          {:id id} entries))
+            (update minutes :time into time)) {:id id} entries))
 
 (defn add-total-minutes [{:keys [time] :as entry}]
   (assoc entry :total-minutes (count time)))
 
 (defn add-most-likely-minute-to-be-asleep [{:keys [time] :as entry}]
-  (->> (frequencies time)
-       (sort-by second)
-       last first
-       (assoc entry :minute-most-asleep)))
+  (let [[minute freq] (->> (frequencies time) (sort-by second) last first)])
+  (assoc entry :minute-most-asleep minute :freq-most-asleep freq))
 
 (defn id*minute-most-asleep [{:keys [id minute-most-asleep]}]
   (* (Integer/parseInt id) minute-most-asleep))
 
-(defn solve-1 []
-  (->> parse-data
+(defn common-work []
+  (->> parsed-data
        (sort-by :time)
        populate-missing-ids
        (remove begins-shift-entry?)
@@ -53,10 +49,17 @@
        (partition 2)
        (map merge-pairs)
        (group-by :id)
-       (map merge-time)
+       (map merge-time)))
+
+(defn solve-1 []
+  (->> (common-work)
        (map add-total-minutes)
        (sort-by :total-minutes) last
        add-most-likely-minute-to-be-asleep
        id*minute-most-asleep))
 
-(defn solve-2 [] nil)
+(defn solve-2 []
+  (->> (common-work)
+       (map add-most-likely-minute-to-be-asleep)
+       (sort-by :freq-most-asleep) last
+       id*minute-most-asleep))
