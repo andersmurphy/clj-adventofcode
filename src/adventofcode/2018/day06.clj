8d1430940e3d0ccc2e7579d64caadd05af89a2c3
anders <andersmurphy@gmail.com>
2018-12-19 20:29:53 +0000

    Solve day06 2018 part 2

--- src/adventofcode/2018/day06.clj
+++ src/adventofcode/2018/day06.clj
@@ -9,7 +9,7 @@
        (reduce (fn [acc [x y]]
                  (conj acc {:x x :y y :id (count acc)})) [])))
 
-(defn parsed-data []
+(def parsed-data
   (-> (slurp (io/resource "adventofcode/2018/day06.txt"))
       (s/split #", |\n")
       data->coordinate-tuples))
@@ -22,26 +22,45 @@
      :max-y (-> by-y last :y)}))
 
 (defn squares [{:keys [min-x max-x min-y max-y]}]
-  (for [x (range min-x max-x)
-        y (range min-y max-y)] {:x x :y y}))
+  (for [x (range min-x (inc max-x))
+        y (range min-y (inc max-y))] {:x x :y y}))
 
 (defn nil-if-equally-far [[{d1 :distance id :id} {d2 :distance}]]
   (if (= d1 d2) nil id))
 
-(defn closest [coords {sqr-x :x sqr-y :y :as m}]
-  (some->> (map (fn [{:keys [x y id]}]
-                  {:id id :distance (+ (math/abs (- x sqr-x))
-                                       (math/abs (- y sqr-y)))})
-                coords)
+(defn distances-to-square [coords {sqr-x :x sqr-y :y}]
+  (map (fn [{:keys [x y id]}]
+         {:id id :distance (+ (math/abs (- x sqr-x))
+                              (math/abs (- y sqr-y)))})
+       coords))
+
+(defn closest [coords square]
+  (some->> (distances-to-square coords square)
            (sort-by :distance)
            nil-if-equally-far
-           (assoc m :closest)))
+           (assoc square :closest)))
+
+(defn total-distance [coords square]
+  (->> (distances-to-square coords square)
+       (map :distance)
+       (apply +)
+       (assoc square :total-distance)))
+
+(defn squares-with [f coords]
+  (->> (bounds coords)
+       squares
+       (keep (partial f coords))))
 
 (defn solve-1 []
-  (let [coords (parsed-data)]
-    (->> (bounds coords)
-         squares
-         (keep (partial closest coords))
-         (group-by :closest)
-         (map (fn [[_ squares]] (count squares)))
-         sort last)))
+  (->> parsed-data
+       (squares-with closest)
+       (group-by :closest)
+       (map (fn [[_ squares]] (count squares)))
+       sort last))
+
+(defn solve-2 []
+  (->> parsed-data
+       (squares-with total-distance)
+       (sort-by :total-distance)
+       (filter (fn [{:keys [total-distance]}] (> 10000 total-distance)))
+       count))
