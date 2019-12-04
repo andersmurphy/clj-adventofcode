2a8aa6dd3aea7176f928b1044fe03f5dba8ec67f
andersmurphy <andersmurphy@gmail.com>
2019-12-04 20:54:11 +0000

    Refactor 2019 day 3

--- src/adventofcode/2019/day03.clj
+++ src/adventofcode/2019/day03.clj
@@ -2,18 +2,20 @@
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
-(def input (slurp "resources/adventofcode/2019/day03.txt"))
-
-(defn ->displacement [string]
+(defn ->line [string]
   (let [direction (subs string 0 1)
         distance  (subs string 1)]
     {:direction direction
      :distance  (Integer/parseInt distance)}))
 
+(defn key-input [[line-a line-b]]
+  {:line-a line-a :line-b line-b})
+
 (defn format-input [input]
   (->> (str/split (str/trim input) #"\n")
        (map #(str/split % #","))
-       (map #(map ->displacement %))))
+       (map #(map ->line %))
+       key-input))
 
 (defn move-in-direction [direction point]
   (let [[axis f] ({"R" [:x inc]
@@ -21,10 +23,9 @@
                    "U" [:y inc]
                    "D" [:y dec]}
                   direction)]
-    (-> (update-in point [:pos axis] f)
-        (update :steps inc))))
+    (update point axis f)))
 
-(defn generate-points [displacements]
+(defn ->points [line]
   (reduce (fn [previous-points
                {:keys [direction distance]}]
             (->> (last previous-points)
@@ -32,42 +33,42 @@
                  (drop 1)
                  (take distance)
                  (into previous-points)))
-          [{:pos   {:x 0 :y 0}
-            :steps 0}]
-          displacements))
+          [{:x 0 :y 0}]
+          line))
 
-(defn find-intersections [[line-a line-b]]
-  (let [line-a-points (generate-points line-a)
-        line-b-points (generate-points line-b)
-        intersections (set/intersection (set (map :pos line-a-points))
-                                        (set (map :pos line-b-points)))]
-    (->> (filter #(-> % :pos intersections) (into line-a-points line-b-points))
-         (group-by :pos)
-         (map (fn [[_ [point {:keys [steps]}]]]
-                (update point :steps + steps))))))
+(defn lines->points [{:keys [line-a line-b] :as m}]
+  (-> (assoc m :line-a (->points line-a))
+      (assoc :line-b (->points line-b))))
+
+(defn find-intersections [{:keys [line-a line-b] :as m}]
+  (->> (set/intersection (set line-a) (set line-b))
+       (assoc m :intersections)))
+
+(defn find-closest-intersection [closest-f input]
+  (->> input
+       format-input
+       lines->points
+       find-intersections
+       closest-f
+       sort
+       second))
+
+(def input (slurp "resources/adventofcode/2019/day03.txt"))
 
 (defn abs [x]
   (if (< x 0) (- x) x))
 
-(defn find-closest-by-manhattan [points]
-  (->> (map (fn [{{:keys [x y]} :pos}]
-              (+ (abs x) (abs y))) points)
-       sort
-       second))
+(defn manhattan-distance [{:keys [intersections]}]
+  (map (fn [{:keys [x y]}]
+         (+ (abs x) (abs y))) intersections))
 
 (defn solve-1 []
-  (->> input
-       format-input
-       find-intersections
-       find-closest-by-manhattan))
+  (find-closest-intersection manhattan-distance input))
 
-(defn find-closest-by-step [points]
-  (->> (map (fn [{:keys [steps]}] steps) points)
-       sort
-       second))
+(defn step-distance [{:keys [intersections line-a line-b]}]
+  (map (fn [point] (+ (.indexOf line-a point)
+                      (.indexOf line-b point)))
+       intersections))
 
 (defn solve-2 []
-  (->> input
-       format-input
-       find-intersections
-       find-closest-by-step))
+  (find-closest-intersection step-distance input))
