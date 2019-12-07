46e4e65a1b2c7f6611e4f91228df84ad622a536c
andersmurphy <andersmurphy@gmail.com>
2019-12-07 10:08:39 +0000

    Refactor 2019 day 6

--- src/adventofcode/2019/day06.clj
+++ src/adventofcode/2019/day06.clj
@@ -16,15 +16,22 @@
    (when-let [nodes (connected-nodes start-node edges)]
      [start-node  (mapv (partial build-graph edges) nodes)])))
 
+(defn build-path-list [graph]
+  (->> (tree-seq second second graph)
+       (map flatten)))
+
+(defn total-orbits [path-list]
+  (->>(map #(-> % count dec) path-list)
+      (reduce +)))
+
 (def input (slurp "resources/adventofcode/2019/day06.txt"))
 
 (defn solve-1 []
   (->> input
        format-input
        build-graph
-       (tree-seq second second)
-       (map #(dec (count (tree-seq second second %))))
-       (reduce +)))
+       build-path-list
+       total-orbits))
 
 (defn length-of-longest-path-containing-a-and-not-b [a b paths]
   (->> (map set paths)
@@ -34,16 +41,15 @@
        count
        dec))
 
-(defn distance-between [node-a node-b tree-s]
-  (let [flat-paths (map flatten tree-s)]
-    (+ (length-of-longest-path-containing-a-and-not-b
-        node-a node-b flat-paths)
-       (length-of-longest-path-containing-a-and-not-b
-        node-b node-a flat-paths))))
+(defn distance-between [node-a node-b path-list]
+  (+ (length-of-longest-path-containing-a-and-not-b
+      node-a node-b path-list)
+     (length-of-longest-path-containing-a-and-not-b
+      node-b node-a path-list)))
 
 (defn solve-2 []
   (->> input
        format-input
        build-graph
-       (tree-seq second second)
+       build-path-list
        (distance-between "YOU" "SAN")))
