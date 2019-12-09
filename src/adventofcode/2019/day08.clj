a1860d024840fb3c40ff265a399d2be89618dd2b
andersmurphy <andersmurphy@gmail.com>
2019-12-09 07:13:34 +0000

    Day 8

---
+++ src/adventofcode/2019/day08.clj
@@ -0,0 +1,27 @@
+(ns adventofcode.2019.day08
+  (:require [clojure.string :as str]))
+
+(def input (slurp "resources/adventofcode/2019/day08.txt"))
+
+(defn format-input [input]
+  (->> (str/split (str/trim input) #"")
+       (map #(Integer/parseInt %))
+       (partition (* 25 6))))
+
+(defn checksum [input]
+  (let [layer-freq-least-zeros (->> (map frequencies input)
+                                    (sort-by #(get % 0))
+                                    first)]
+    (* (layer-freq-least-zeros 1)
+       (layer-freq-least-zeros 2))))
+
+(defn solve-1 []
+  (->> input
+       format-input
+       checksum))
+
+(defn solve-2 []
+  (->> input
+       format-input
+       (apply map (fn [& args] (some #{1 0} args)))
+       (partition 25)))
